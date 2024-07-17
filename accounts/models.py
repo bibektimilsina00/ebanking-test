361f590834d77bcada0f0cee29db199ddba580aa
Bibek Timilsina <bibektimilsina000@gmail.com>
2024-07-18 00:15:18 +0545

    chore: Update CustomUser model fields in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -5,34 +5,34 @@ from django_cryptography.fields import encrypt
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email =  (models.EmailField(unique=True))
-    phone = encrypt(models.CharField(max_length=15, blank=True))
-    first_name =  (models.CharField(max_length=30, blank=True))
-    last_name =  (models.CharField(max_length=30, blank=True))
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, unique=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
     address = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     password_changed = models.BooleanField(default=False)
-    username =  models.CharField(max_length=30, blank=True,null=True)
-    avtar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    username = models.CharField(max_length=30, blank=True, null=True)
+    avtar = models.ImageField(upload_to="avatars/", blank=True, null=True)
 
     ROLE_CHOICES = [
-        ('superadmin', 'SuperAdmin'),
-        ('organization', 'Organization'),
-        ('branch', 'Branch'),
-        ('staff', 'Staff'),
+        ("superadmin", "SuperAdmin"),
+        ("organization", "Organization"),
+        ("branch", "Branch"),
+        ("staff", "Staff"),
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = "User"
+        verbose_name_plural = "Users"
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
